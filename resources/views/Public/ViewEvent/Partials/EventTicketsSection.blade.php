49ae55d903be32c134a3d15d9b94668dde027e33
brompo <brompo@gmail.com>
2018-03-22 15:49:53 +0300

    fixed spacing on tickets

--- resources/views/Public/ViewEvent/Partials/EventTicketsSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventTicketsSection.blade.php
@@ -127,7 +127,7 @@
                                       </td>
                                       <?php } ?>
                                       <!--end of addition-->
-                                      <td class="col-md-2" text-align: right;">
+                                      <td class="col-md-3" text-align: right;">
                                           <div class="ticket-pricing" style="margin-right: 20px;">
                                               @if($ticket->is_free)
                                                   FREE
@@ -144,7 +144,7 @@
                                               @endif
                                           </div>
                                       </td>
-                                      <td class="col-md-4">
+                                      <td class="col-md-3">
                                           @if($ticket->is_paused)
 
                                               <span class="text-danger">
