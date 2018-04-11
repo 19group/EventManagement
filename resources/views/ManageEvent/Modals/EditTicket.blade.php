58a3f0e51c3f50d531383a78fb6efa1b2adb4d4b
brompo <brompo@gmail.com>
2018-04-11 09:44:26 +0300

    removed dependency on accommodation table, as its not used

--- resources/views/ManageEvent/Modals/EditTicket.blade.php
+++ resources/views/ManageEvent/Modals/EditTicket.blade.php
@@ -177,11 +177,11 @@ document.getElementById("ticketoffers").appendChild(p);
                         </div>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row more-options">
                     <div class="col-md-12">
                         <div class="form-group">
                              {!! Form::label('type', 'Ticket Type', array('class'=>' control-label')) !!}
-                             {!! Form::select('type', ['normal'=>'Normal', 'extra'=>'Extra'],null, ['class' => 'form-control']) !!}
+                             {!! Form::select('type', ['normal'=>'Normal'],null, ['class' => 'form-control']) !!}
 
                         </div>
                     </div>
