1f0448b124f64761654dd38bc6f4b6bf5f6e5ea5
Donald-Paul <don2paul09@gmail.com>
2018-05-16 09:43:39 +0300

    sales volume and tickets sold separations... all events sales volume yet to work

--- resources/views/ManageOrganiser/Partials/EventPanel.blade.php
+++ resources/views/ManageOrganiser/Partials/EventPanel.blade.php
@@ -1,3 +1,17 @@
+@php $ticketcount=0; $othercount=0; $ticketsales=0; $othersales=0; $totaldonation=0;@endphp
+@foreach($event->tickets as $ticke) 
+    @if(!in_array($ticke->type, ['SIDEEVENT','extra','extras'])) 
+        @php $ticketcount += $ticke->quantity_sold; $ticketsales+=$ticke->price; @endphp
+    @else
+        @php $othercount += $ticke->quantity_sold; $othersales+=$ticke->price; @endphp
+    @endif
+@endforeach 
+@foreach ($event->orders as $donorder)
+    @php $orderwithdonation= \App\Models\OrderItem::where(['order_id'=>$donorder->id, 'title'=>'Donation'])->first(); @endphp
+    @if(count($orderwithdonation) != 0)
+        @php $totaldonation += $orderwithdonation->unit_price; @endphp
+    @endif
+@endforeach
 <div class="panel panel-success event rounded">
     <div class="panel-heading" data-style="background-color: {{{$event->bg_color}}};background-image: url({{{$event->bg_image_url}}}); background-size: cover;">
         <div class="event-date">
@@ -26,13 +40,14 @@
             <li>
                 <div class="section">
                     <h4 class="nm">{{$event->tickets->sum('quantity_sold')}}</h4>
-                    <p class="nm text-muted">Tickets Sold</p>
+                    <p class="nm text-muted">All Tickets Sold</p>
                 </div>
             </li>
 
             <li>
                 <div class="section">
-                    <h4 class="nm">{{{money($event->sales_volume + $event->organiser_fees_volume, $event->currency)}}}</h4>
+                    <!--h4 class="nm">{{{money($event->sales_volume + $event->organiser_fees_volume, $event->currency)}}}</h4-->
+                    <h4 class="nm">{{{money($ticketsales + $othersales + $totaldonation, $event->currency)}}}</h4>
                     <p class="nm text-muted">Revenue</p>
                 </div>
             </li>
